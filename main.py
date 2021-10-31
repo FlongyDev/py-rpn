f13a8b75a6556de10be0bb79f78d53f01899b0c9
Flongy <craftbymax@gmail.com>
2021-10-31 11:04:58 +0300

    Intermediate representation for RPN

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 """ Обратная польская запись: калькулятор """
 from typing import Callable
 
-all_ops: dict[str, Callable[[list], int]] = {}
+TOKEN_TYPE = int
+INTERMEDIATE = list[tuple[TOKEN_TYPE]]
+
+# Enum
+PUSH = 0
+ADDITION = 1
+SUBTRACTION = 2
+MULTIPLICATION = 3
+DIVISION = 4
+MODULO = 5
+POWER = 6
+
+all_ops: dict[TOKEN_TYPE, Callable[[list], int]] = {}
+all_tokens: dict[str, TOKEN_TYPE] = {}
 
 
-def binary_op(token: str) -> Callable[[Callable[[int, int], int]], Callable[[list], int]]:
+def binary_op(str_token: str, token: TOKEN_TYPE) -> Callable[[Callable[[int, int], int]], Callable[[list], int]]:
     def make_binop(func: Callable[[int, int], int]) -> Callable[[list], int]:
         def redef(stack: list) -> int:
             if len(stack) < 2:
@@ -18,53 +31,71 @@ def binary_op(token: str) -> Callable[[Callable[[int, int], int]], Callable[[lis
 
             return result
         all_ops[token] = redef
+        all_tokens[str_token] = token
         return redef
     return make_binop
 
 
 # add = binary_op('+')(add)
-@binary_op('+')
+@binary_op('+', ADDITION)
 def add(a, b):
     return a + b
 
 
-@binary_op('-')
+@binary_op('-', SUBTRACTION)
 def sub(a, b):
     return a - b
 
 
-@binary_op('*')
+@binary_op('*', MULTIPLICATION)
 def mul(a, b):
     return a * b
 
 
-@binary_op('//')
+@binary_op('//', DIVISION)
 def div(a, b):
     return a // b
 
 
-@binary_op('%')
+@binary_op('%', MODULO)
 def mod(a, b):
     return a % b
 
 
-@binary_op('^')
+@binary_op('^', POWER)
 def power(a, b):
     return a ** b
 
 
-# "3 4 +"
-def execute_program(program: str) -> int:
-    stack = []
-    for token in program.split():
-        operation = all_ops.get(token, None)
-        if operation is not None:
-            operation(stack)
+def parse_str_postfix(rpn: str) -> INTERMEDIATE:
+    token_list = []
+    for str_token in rpn.split():
+        token = all_tokens.get(str_token, None)
+        if token is not None:
+            token_list.append((token,))
         else:
             try:
-                stack.append(int(token))
+                value = int(str_token)
+                token_list.append((PUSH, value))
             except ValueError:
-                raise ValueError(f"{token!r} - неизвестная операция или не целое число")
+                raise ValueError(f"{str_token!r} - неизвестная операция или не целое число")
+
+    return token_list
+
+
+def execute_program(program: list[tuple[TOKEN_TYPE]]) -> int:
+    stack = []
+    for op in program:
+        token = op[0]
+        if token == PUSH:
+            stack.append(op[1])
+            continue
+
+        func = all_ops.get(token, None)
+        if func is None:
+            raise ValueError(f"Операция {token} не реализована!")
+
+        func(stack)
 
     if len(stack) != 1:
         raise ValueError(f"Ошибка! К концу вычислений на стаке осталось не одно число: {stack}")
@@ -73,5 +104,5 @@ def execute_program(program: str) -> int:
 
 
 if __name__ == "__main__":
-    print(execute_program("15 7 %"), "==", 1)
-    print(execute_program("3 6 3 * 1 4 - 2 ^ // +"), "==", 5)
+    print(execute_program(parse_str_postfix("15 7 %")), "==", 1)
+    print(execute_program(parse_str_postfix("3 6 3 * 1 4 - 2 ^ // +")), "==", 5)
